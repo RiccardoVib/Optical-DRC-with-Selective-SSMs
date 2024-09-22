fbc939302ac9d47c9ad4d5db5d9f47ef1f38679b
RiccardoVib <riccardo.simionato.vib@gmail.com>
2024-09-22 16:04:23 +0200

    Update starter.py

--- Code/starter.py
+++ Code/starter.py
@@ -1,19 +1,24 @@
 #from Training_baseline import train
 from Training import train
 
+"""
+main script
 
+"""
+
+# data_dir: the directory in which datasets are stored
 data_dir = '../../Files/'
 epochs = [1, 200]
-units = 6
-batch_size = 600*4
-lr = 3e-4
+units = 6 # number of model's units
+batch_size = 600*4 # batch size
+lr = 3e-4 # initial learning rate
 
 name_model = ''
 
 comp = 'CL1B'
 dataset = ''
 
-model = 'S4D'
+models = ['LSTM', 'ED', 'S4D', S 
 train(data_dir=data_dir,
       save_folder=model+comp+dataset+name_model,
       dataset=comp+'/'+comp+dataset,
