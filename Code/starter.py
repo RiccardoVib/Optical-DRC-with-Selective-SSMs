d64e0693abd572371294eca21c2579f045f09917
RiccardoVib <riccardo.simionato.vib@gmail.com>
2024-09-22 16:05:59 +0200

    Update starter.py

--- Code/starter.py
+++ Code/starter.py
@@ -8,7 +8,7 @@ main script
 
 # data_dir: the directory in which datasets are stored
 data_dir = '../../Files/'
-epochs = [1, 200]
+epochs = 200
 units = 6 # number of model's units
 batch_size = 600*4 # batch size
 lr = 3e-4 # initial learning rate
@@ -18,14 +18,16 @@ name_model = ''
 comp = 'CL1B'
 dataset = ''
 
-models = ['LSTM', 'ED', 'S4D', S 
-train(data_dir=data_dir,
-      save_folder=model+comp+dataset+name_model,
-      dataset=comp+'/'+comp+dataset,
-      comp=comp,
-      batch_size=batch_size,
-      learning_rate=lr,
-      units=units,
-      epochs=epochs,
-      model=model,
-      inference=False)
+models = ['LSTM', 'ED', 'S4D', 'S6'] 
+#models = ['LSTMb', 'EDb'] 
+for model in models:
+      train(data_dir=data_dir,
+            save_folder=model+comp+dataset+name_model,
+            dataset=comp+'/'+comp+dataset,
+            comp=comp,
+            batch_size=batch_size,
+            learning_rate=lr,
+            units=units,
+            epochs=epochs,
+            model=model,
+            inference=False)
