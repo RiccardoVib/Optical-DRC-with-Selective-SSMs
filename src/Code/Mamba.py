2f3d7ecdf3c11a822e5169eeea9cad72af9ac627
Riccardo <riccardo.simionato.vib@gmail.com>
2026-08-18 15:41:24 +0200

    Update Mamba.py

--- src/Code/Mamba.py
+++ src/Code/Mamba.py
@@ -27,62 +27,84 @@ import math
 code adapted from https://github.com/state-spaces/mamba & https://github.com/PeaBrane/mamba-tiny
 '''
 
+_EPS = tf.constant(1e-12, dtype=tf.float32)
+
+
 def selective_scan(u, delta, A, B, C, D, last_state, stateful):
+    u = tf.convert_to_tensor(u)
+    delta = tf.cast(delta, u.dtype)
+    A = tf.cast(A, u.dtype)
+    B = tf.cast(B, u.dtype)
+    C = tf.cast(C, u.dtype)
+    D = tf.cast(D, u.dtype)
+
     dA = tf.einsum('bld,dn->bldn', delta, A)
     dB_u = tf.einsum('bld,bld,bln->bldn', delta, u, B)
 
-    dA_cumsum = tf.pad(dA[:, 1:], [[0, 0], [1, 0], [0, 0], [0, 0]])
-    dA_cumsum = tf.math.cumsum(dA_cumsum, axis=1)
-    dA_cumsum = tf.exp(dA_cumsum)
+    padded_dA = tf.pad(dA[:, 1:], [[0, 0], [1, 0], [0, 0], [0, 0]])
+    cumulative_A = tf.math.cumsum(padded_dA, axis=1)
+    cumulative_A_exp = tf.exp(cumulative_A)
 
-    x = dB_u / (dA_cumsum + 1e-12)
-    x = tf.math.cumsum(x, axis=1) * dA_cumsum
+    states = tf.cumsum(dB_u / (cumulative_A + _EPS), axis=1) * cumulative_A
 
-    if stateful:
-        dA_cumsum_l = tf.math.cumsum(dA, axis=1)
-        dA_cumsum_l = tf.exp(dA_cumsum_l)
-        dA_cumsum_l *= tf.expand_dims(last_state, axis=1)
-        x = x + dA_cumsum_l
+    if stateful and last_state is not None:
+        initial_A = tf.exp(tf.cumsum(dA, axis=1))
+        states = states + initial_A * last_state[:, None, :, :]
 
-    last_state = x[:, -1]
-    y = tf.einsum('bldn,bln->bld', x, C)
-
-    return y + u * D, last_state
+    final_state = states[:, -1]
+    output = tf.einsum("bldn,bln->bld", states, C)
+    return output + u * D, final_state
 
 
-class MambaBlock(tf.keras.layers.Layer):
-    def __init__(self, layer_id, model_input_dims, model_internal_dim, conv_use_bias, dense_use_bias, conv_kernel_size, delta_t_rank, model_states, batch_size, mini_batch_size, stateful, **kwargs):
+class MambaLay(tf.keras.layers.Layer):
+    def __init__(self,  
+        model_input_dims,
+        projection_expand_factor,
+        conv_kernel_size,
+        delta_t_rank,
+        model_states,
+        batch_size,
+        mini_batch_size,
+        conv_use_bias=True,
+        dense_use_bias=True,
+        stateful=True
+        ):
         super(MambaBlock, self).__init__(**kwargs)
-        self.layer_id = layer_id
-        self.model_internal_dim = model_internal_dim
-        self.model_input_dims = model_input_dims
-        self.conv_use_bias = conv_use_bias
-        self.dense_use_bias = dense_use_bias
-        self.conv_kernel_size = conv_kernel_size
-        self.delta_t_rank = delta_t_rank
-        self.model_states = model_states
+        self.model_input_dims = int(model_input_dims)
+        self.model_internal_dim = int(projection_expand_factor * model_input_dims)
+        self.delta_t_rank = math.ceil(model_input_dims / 2)  # 16            
+        self.conv_kernel_size = int(conv_kernel_size)
+        self.model_states = int(model_states)
         self.batch_size = batch_size
         self.mini_batch_size = mini_batch_size
-        self.stateful = stateful
+        self.stateful = bool(stateful)
+        self._state: Optional[tf.Variable] = None
 
         self.in_projection = tf.keras.layers.Dense(
-            self.model_internal_dim * 2,
-            input_shape=(self.model_input_dims,), use_bias=False, trainable=True)
-
+            2 * self.model_internal_dim,
+            use_bias=False,
+        )
         self.conv1d = tf.keras.layers.Conv1D(
             filters=self.model_internal_dim,
-            use_bias=self.conv_use_bias,
             kernel_size=self.conv_kernel_size,
             groups=self.model_internal_dim,
-            data_format='channels_first',
-            padding='causal', trainable=True
+            use_bias=conv_use_bias,
+            data_format="channels_first",
+            padding="causal",
         )
-
-        self.x_projection = tf.keras.layers.Dense(self.delta_t_rank + self.model_states * 2, use_bias=False, trainable=True)
-
-        self.delta_t_projection = tf.keras.layers.Dense(self.model_internal_dim,
-                                               input_shape=(self.delta_t_rank,), use_bias=True, trainable=True)
-
+        self.x_projection = tf.keras.layers.Dense(
+            self.delta_t_rank + 2 * self.model_states,
+            use_bias=False,
+        )
+        self.delta_t_projection = tf.keras.layers.Dense(
+            self.model_internal_dim,
+            use_bias=True,
+        )
+        self.out_projection = tf.keras.layers.Dense(
+            self.model_input_dims,
+            use_bias=dense_use_bias,
+        )
+            
         self.A = repeat(
             tf.range(1, self.model_states + 1, dtype=tf.float32),
             'n -> d n', d=self.model_internal_dim)
@@ -100,11 +122,23 @@ class MambaBlock(tf.keras.layers.Layer):
         self.out_projection = tf.keras.layers.Dense(
             self.model_input_dims,
             input_shape=(self.model_internal_dim,),
-            use_bias=self.dense_use_bias, trainable=True)
+            use_bias=self.dense_use_bias)
         self.reset_states()
 
+    def _ensure_state(self, batch_size):
+        shape = (batch_size, self.model_internal_dim, self.model_states)
+        if self._state is None or self._state.shape != tf.TensorShape(shape):
+            self._state = self.add_weight(
+                name="state",
+                shape=shape,
+                initializer="zeros",
+                trainable=False,
+            )
+        return self._state
+            
     def reset_states(self):
-        self.state = tf.Variable(tf.zeros((9, self.model_internal_dim, self.model_states), dtype=tf.float32), name='state', trainable=False)
+        if self._state is not None:
+            self._state.assign(tf.zeros_like(self._state))
 
     def call(self, x):
 
@@ -117,38 +151,43 @@ class MambaBlock(tf.keras.layers.Layer):
         x = rearrange(x, 'b l d_in -> b d_in l')
         x = self.conv1d(x)[:, :, :self.mini_batch_size]
         x = rearrange(x, 'b d_in l -> b l d_in')
-
         x = tf.nn.swish(x)
 
-        last_state = self.state[:self.batch_size]
-        res_state = self.state[self.batch_size:]
-
-        y, y_state = self.ssm(x, last_state=last_state, stateful=self.stateful)
-
+        state = self._ensure_state(tf.shape(x)[0])
+        y, final_state = self.ssm(
+            x,
+            last_state=state,
+            stateful=self.stateful,
+        )
         if self.stateful:
-            self.state.assign(tf.concat([y_state, res_state], axis=0))
-        y = y * tf.nn.swish(res)
+            state.assign(final_state)
+
+        y = y * tf.nn.swish(residual)
         return self.out_projection(y)
 
     def ssm(self, x, last_state, stateful):
 
-        (d_in, n) = self.A_log.shape
-
-        A = -tf.exp(tf.cast(self.A_log, tf.float32))  # shape -> (d_in, n)
-        D = tf.cast(self.D, tf.float32)
+        A = -tf.exp(tf.cast(self.A_log, x.dtype))  # shape -> (d_in, n)
         x_dbl = self.x_projection(x)  # shape -> (batch, seq_len, delta_t_rank + 2*n)
-        (delta, B, C) = tf.split(
+
+        delta, B, C = tf.split(
             x_dbl,
-            num_or_size_splits=[self.delta_t_rank, n, n],
-            axis=-1)  # delta.shape -> (batch, seq_len) & B, C shape -> (batch, seq_len, n)
-
-
+            [self.delta_t_rank, self.model_states, self.model_states],
+            axis=-1,
+        )   # delta.shape -> (batch, seq_len) & B, C shape -> (batch, seq_len, n)
+               
         delta = tf.nn.softplus(self.delta_t_projection(delta))  # shape -> (batch, seq_len, model_input_dim)
 
-        y, y_state = selective_scan(x, delta, A, B, C, D, last_state, stateful)
-
-        return y, y_state
-
+       return selective_scan(
+            x,
+            delta,
+            A,
+            B,
+            C,
+            self.D,
+            last_state,
+            stateful,
+        )
 
 class MambaLay(tf.keras.layers.Layer):
     def __init__(self, model_states, projection_expand_factor=2, model_input_dims=2, conv_kernel_size=4, batch_size=9, mini_batch_size=2400, stateful=False, type=tf.float32):
